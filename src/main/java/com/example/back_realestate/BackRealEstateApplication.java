405789908f9c2b8f0823635e02805fe481a97ddf
nikit <nikita.melamed@gmail.com>
2023-04-13 20:51:44 +0400

    Initial commit

---
+++ src/main/java/com/example/back_realestate/BackRealEstateApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_realestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackRealEstateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackRealEstateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/back_realestate/BackRealEstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back_realestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRealEstateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
